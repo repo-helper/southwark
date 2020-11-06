eebec127dba715d31317c6c9d46bdab72d111f20
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2020-11-06 15:29:39 +0000

    Bump version v0.3.1 -> v0.4.0

--- southwark/__init__.py
+++ southwark/__init__.py
@@ -61,7 +61,7 @@ from typing_extensions import TypedDict
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2020 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.3.1"
+__version__: str = "0.4.0"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = [
