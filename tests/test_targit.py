c1604f8dbc794ede431afb8b3af7ecab8cad16af
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-01-30 23:07:11 +0000

    Improve test coverage.

--- tests/test_targit.py
+++ tests/test_targit.py
@@ -5,11 +5,12 @@ import socket
 from pathlib import Path
 
 # 3rd party
+import pytest
 from apeye.requests_url import RequestsURL
 from domdf_python_tools.paths import PathPlus
 
 # this package
-from southwark.targit import TarGit
+from southwark.targit import BadArchiveError, TarGit, check_archive_paths
 
 logo_url = "https://upload.wikimedia.org/wikipedia/commons/c/c3/Python-logo-notext.svg"
 python_logo = RequestsURL(logo_url).get().content
@@ -21,6 +22,7 @@ def test_targit(tmp_pathplus, monkeypatch):
 
 	t = TarGit(tmp_pathplus / "file.tar.gz", 'w')
 	assert (tmp_pathplus / "file.tar.gz.lock").is_file()
+	assert t.mode == 'w'
 
 	assert isinstance(t / "foo.txt", Path)
 	assert isinstance(t / "foo.txt", PathPlus)
@@ -29,25 +31,27 @@ def test_targit(tmp_pathplus, monkeypatch):
 
 	assert t.status() == {"add": [PathPlus("foo.txt")], "delete": [], "modify": []}
 
-	t.save()
+	assert t.save()
 	assert t.status() == {"add": [], "delete": [], "modify": []}
 
 	(t / "logo.svg").write_bytes(python_logo)
 	assert t.status() == {"add": [PathPlus("logo.svg")], "delete": [], "modify": []}
 
-	t.save()
+	assert t.save()
 	assert t.status() == {"add": [], "delete": [], "modify": []}
 
 	t.close()
 
 	t = TarGit(tmp_pathplus / "file.tar.gz", 'a')
 	assert t.status() == {"add": [], "delete": [], "modify": []}
+	assert t.mode == 'a'
 
 	(t / "foo.txt").write_clean("Hello\nEveryone")
 	assert t.status() == {"add": [], "delete": [], "modify": [PathPlus("foo.txt")]}
 
-	t.save()
+	assert t.save()
 	assert t.status() == {"add": [], "delete": [], "modify": []}
+	assert not t.save()
 
 	assert str(t).endswith("file.tar.gz")
 	assert re.match(r"TarGit\(.*file\.tar\.gz\)", repr(t))
@@ -62,3 +66,33 @@ def test_targit(tmp_pathplus, monkeypatch):
 	assert len(list(t.history)) == 4
 
 	t.close()
+
+	with pytest.raises(FileExistsError, match="TarGit file '.*' already exists."):
+		TarGit(tmp_pathplus / "file.tar.gz", 'w')
+
+	with pytest.raises(ValueError, match="Unknown IO mode 'wb'"):
+		TarGit(tmp_pathplus / "file.tar.gz", "wb")  # type: ignore
+
+	with pytest.raises(ValueError, match="Unknown IO mode 't'"):
+		TarGit(tmp_pathplus / "file.tar.gz", 't')  # type: ignore
+
+
+def test_check_archive_paths():
+
+	class Archive:
+
+		def getnames(self):
+			return ["foo/bar/baz.py", "code.c"]
+
+	assert check_archive_paths(Archive())  # type: ignore
+
+	class Archive:  # type: ignore
+
+		def getnames(self):
+			return ["/usr/bin/malware.sh", "~/.local/bin/malware.sh", "./.local/bin/malware.sh"]
+
+	with pytest.raises(
+			BadArchiveError,
+			match="Refusing to extract an archive containing files utilising path traversal.",
+			):
+		check_archive_paths(Archive())  # type: ignore
